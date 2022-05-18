1acee1a6378e6d9a99468d5b52f35e4e0cf44f93
Silas Peters <silaspeters03@gmail.com>
2022-05-18 11:41:22 +0200

    fix, hopelijk

--- Opdracht 2/pages/echo.vue
+++ Opdracht 2/pages/echo.vue
@@ -1,27 +1,27 @@
 <template>
-  <div>
-    <h1>Echo...</h1>
+	<div>
+		<h1>Echo...</h1>
 		<form v-on:submit.prevent="submit"> <!--voorkomt een redirection waardoor de state verloren raakt, maar voert wel submit() uit-->
 			<label>Wat zijn je favoriete dieren?</label><br>
-    		<input type="text" name="animal1" id="animal1Field"><br>
-    		<input type="text" name="animal2" id="animal2Field"><br>
+				<input type="text" name="animal1" id="animal1Field"><br>
+				<input type="text" name="animal2" id="animal2Field"><br>
 			<label for="email">Wat is je hoogst persoonlijke email?</label><br>
-    		<input type="email" name="email" id="emailField"><br><br>
+				<input type="email" name="email" id="emailField"><br><br>
 			<input type="submit" value="Hoor mij!">
 		</form>
 
 		<br><br>
 		<p>> Echo response:</p>
 		<p>{{echo}}</p>
-  </div>
+	</div>
 </template>
 
 <script>
 export default {
 	computed: {
-   	echo() {
-     	return this.$store.state.echoResponse;
-   	}
+	 	echo() {
+		 	return this.$store.state.echoResponse;
+	 	}
  	},
 	methods: {
 		submit() {
