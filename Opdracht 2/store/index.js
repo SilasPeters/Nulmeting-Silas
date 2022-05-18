ab11e9c2a010de635b432b9bc5233b2494019a81
Silas Peters <silaspeters03@gmail.com>
2022-05-18 11:09:06 +0200

    API Call

--- Opdracht 2/store/index.js
+++ Opdracht 2/store/index.js
@@ -1,38 +1,40 @@
 export const state = () => ({
-  count: 0,
-  messages: [],
+	count: 0,
+	messages: [],
 	echoResponse: "",
 });
 export const mutations = {
-  increment(state) {
-    state.count++;
-  },
-  addMessage(state, newMessage) {
-    state.messages.push(newMessage);
-  },
-  clear(state) {
-    state.messages = [];
-  },
+	increment(state) {
+		state.count++;
+	},
+	addMessage(state, newMessage) {
+		state.messages.push(newMessage);
+	},
+	clear(state) {
+		state.messages = [];
+	},
 	setEchoResponse(state, responseData){
 		state.echoResponse = responseData;
 	},
 };
 export const actions = {
-  async echo({ commit }, formData) {
-    let data = {
-      echo: formData,
-    };
-    let options = {
-      url: "https://d5gryl2o0g.execute-api.eu-west-1.amazonaws.com/development/echo",
-      method: "POST",
-      headers: {
-        "x-api-key": process.env.API_KEY,
-      },
-      data,
-    };
-    //alert(JSON.stringify(options));
-    let response = await this.$axios(options);//this.$axios.$get(options);
-    alert(response);
-    commit("setEchoResponse", response);
-  },
-};
+	async echo ({ commit }, formData) {
+		let data = {
+			echo: formData
+		}
+		let options = {
+			url: "https://d5gryl2o0g.execute-api.eu-west-1.amazonaws.com/development/echo",
+			method: "POST",
+			headers: {
+				"x-api-key": process.env.API_KEY
+			},
+			data
+		}
+		alert(JSON.stringify(options));
+		let response = await this.$axios(options);
+		// De alerts hieronder worden nooit aangeroepen
+		alert(response);
+		alert(response.data);
+		commit('setEchoResponse', response.data);
+	}
+}
